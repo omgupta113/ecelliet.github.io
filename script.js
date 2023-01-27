a138f3042dcd53f0cec8700b1cbc79766bedd281
omgupta113 <omgupta113@gmail.com>
2023-01-27 23:58:31 +0530

    j

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load",function(){
     loader.style.display = "none";
 })
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
