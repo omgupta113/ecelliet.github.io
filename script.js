a409b023c8b7ad0f95a99385a7fc5f19853e2c12
omgupta113 <omgupta113@gmail.com>
2023-01-27 23:43:27 +0530

    n

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-function say(a){
-    var n=a.length;
-    let word='';
-    for(let i = 0;n>i;i++){
-        word = word + a[i];
-        console.log(word);
-    }
-}
-while(true){
-    var a="innovation";
-    say(a);
-    var b="incubation";
-    say(b);
-    var c="ideation";
-    say(c);
-}
+var loader = document.getElementById("preloader");
+window.addEventListener("load",function(){
+    loader.style.display = "none";
+})
